4780f1e108a931b58e8c2c172bc9bc8e10f7939d
Saif Abrar <saifabrar.1998@gmail.com>
2025-02-02 17:37:11 +0600

    add task button's business logic basic

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -1,16 +1,21 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    tasks : [],
-}
+  tasks: [],
+};
 const tasksSlice = createSlice({
-    name: "tasksSlice",
-    initialState,
-    reducers:{
-        addTask : (state, {payload})=> {
-            state.tasks.push(payload)
-        }
+  name: "tasksSlice",
+  initialState,
+  reducers: {
+    addTask: (state, { payload }) => {
+      if (state.tasks.length == 0) {
+        state.tasks.push({ id: 1, ...payload });
+      } else {
+        const lastElement = state.tasks.at(-1);
+        state.tasks.push({ id: lastElement.id + 1, ...payload });
+      }
     },
+  },
 });
-export const {addTask} = tasksSlice.actions;
+export const { addTask } = tasksSlice.actions;
 export default tasksSlice.reducer;
